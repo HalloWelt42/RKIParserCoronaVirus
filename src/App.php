0b7790a38c780cb445cce8de31e703dfc387745a
Marcel Papp <github@texeln.de>
2020-03-06 16:30:52 +0100

    2. init

---
+++ src/App.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace HEF;
+
+class App
+{
+
+    public function __construct ()
+    {
+
+        $url = 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html';
+        $data = file_get_contents( $url );
+        $dom = new \DOMDocument( '1.0' , 'utf-8' );
+        $dom -> loadHTML( $data );
+        $domx = new \DOMXPath( $dom );
+        $tables = $domx -> query( '//table/tbody/.' ) -> item( 0 );
+
+        /**
+         * @var $node \DOMNode
+         */
+        foreach ( $tables -> childNodes as $node ) {
+            $bundesland = $node -> childNodes -> item( 0 ) -> textContent;
+            $faelle = $node -> childNodes -> item( 1 ) -> textContent;
+            print_r( "{$bundesland} {$faelle}" );
+            print_r( PHP_EOL );
+        }
+
+    }
+
+
+}
---
+++ src/start.php
@@ -0,0 +1,6 @@
+<?php
+
+use HEF\App;
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'loader.php';
+new App();
